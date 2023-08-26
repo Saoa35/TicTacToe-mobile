ac1cdf62e315c0587b9a842260c729476263e80b
Saoa35 <saoamah@gmail.com>
2023-08-26 15:24:57 +0400

    * edited Circle comp.

--- src/components/Circle.jsx
+++ src/components/Circle.jsx
@@ -3,26 +3,17 @@ import { StyleSheet, View } from "react-native";
 import { COLORS } from "../constants/themes";
 
 export const Circle = () => {
-  return (
-    <View style={styles.circle}>
-      <View style={styles.innerCircle}></View>
-    </View>
-  );
+  return <View style={styles.circle}></View>;
 };
 
 const styles = StyleSheet.create({
   circle: {
     width: 100,
     height: 100,
-    backgroundColor: COLORS.items,
+    borderWidth: 15,
+    borderColor: COLORS.items,
     borderRadius: 999,
     alignItems: "center",
     justifyContent: "center",
-  },
-  innerCircle: {
-    width: 70,
-    height: 70,
-    backgroundColor: COLORS.background,
-    borderRadius: 9999,
   },
 });
